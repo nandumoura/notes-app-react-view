6bd8417f2c5d65b7f4f4d5d64bb6908cb2ef5c2d
Fernando Moura <nandumoura@gmail.com>
2022-04-29 18:35:00 -0300

    feat: create header componnent

---
+++ src/componnents/header/header.js
@@ -0,0 +1,24 @@
+import "./header.css"
+import Input from "../input/input"
+import { HiOutlineChevronLeft, HiDotsCircleHorizontal } from 'react-icons/hi';
+
+
+function Header() {
+    return (
+        <header>
+            <div className="navigation-content">
+                <div className="navigation-item">
+                   <a href="/back"> <HiOutlineChevronLeft /><p>Back</p></a>
+                </div>
+                <div className="navigation-item">
+                    <p>Home</p></div>
+                <div className="navigation-item"><HiDotsCircleHorizontal /></div>
+            </div>
+            <div className="search-content">
+                <Input placeholder="Search" />
+            </div>
+        </header>
+    )
+}
+
+export default Header
