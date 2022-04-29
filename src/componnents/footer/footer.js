5ec71949d34e1f62fb60c19a3890feb60ce4a53c
Fernando Moura <nandumoura@gmail.com>
2022-04-29 18:52:26 -0300

    feat: create footer component

---
+++ src/componnents/footer/footer.js
@@ -0,0 +1,21 @@
+import "./footer.css"
+import { HiHome, HiOutlineCollection } from 'react-icons/hi';
+import { GiNotebook } from "react-icons/gi"
+
+function Footer() {
+    return (
+        <div className="footer-content">
+            <div className="footer-item">
+                <HiHome />
+            </div>
+            <div className="footer-item">
+                <GiNotebook />
+            </div>
+            <div className="footer-item">
+                <HiOutlineCollection />
+            </div>
+        </div>
+    )
+}
+
+export default Footer
