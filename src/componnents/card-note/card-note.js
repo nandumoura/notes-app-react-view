9f6d03283e9f90842741b8ec4981d68c7655ab6f
Fernando Moura <nandumoura@gmail.com>
2022-04-29 18:34:29 -0300

    feat: change card to acept new colors

--- src/componnents/card-note/card-note.js
+++ src/componnents/card-note/card-note.js
@@ -1,10 +1,11 @@
 import "./card-note.css"
 
 function CardNote(props) {
-
+    const classes = `cardNote ${props.color}`
     return (
-        <div className="cardNote">
+        <div className={classes}>
             <h2>{props.title}</h2>
+            <p>{props.text}</p>
         </div>
     )
 }
