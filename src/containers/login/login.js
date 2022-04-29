817c4dbafbed7cfe2c819397f5228b1860d2d5e5
Fernando Moura <nandumoura@gmail.com>
2022-04-29 14:21:32 -0300

    feat: add login container

---
+++ src/containers/login/login.js
@@ -0,0 +1,26 @@
+import "./login.css"
+import CardNote from "../../componnents/card-note/card-note";
+import Input from "../../componnents/input/input";
+import Button from "../../componnents/button/button";
+
+
+function Login() {
+  return (
+    <div className="Login">
+      <main>
+        <CardNote title="Jot" />
+        <p className="text-login">Let's make some notes!</p>
+        <div className="input-container">
+          <Input placeholder="User Name" />
+          <Input placeholder="Password" />
+          <a href="/forgot"> Forgot Password</a>
+          <Button text="Sign in" />
+          <p className="sing-up-text">Don't have an account?  <a href="/forgot"> SIGN UP</a></p>
+         
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Login;
